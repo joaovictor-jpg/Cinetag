42f00c0b35e395ff87a797ea479fc6ecdd83e431
João Victor <joaovictormdasilva676@gmail.com>
2024-08-26 14:01:33 -0300

    Criando cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,24 @@
 import Cabecalho from "componentes/cabecalho";
-import Styles from "./Inicio.module.css";
 import Rodape from "componentes/rodape";
+import Banner from "componentes/banner";
+import Titulo from "componentes/titulo";
+import Card from "componentes/card";
+import styles from "./Inicio.module.css";
+import videos from "../../json/db.json";
 
 export default function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     );
